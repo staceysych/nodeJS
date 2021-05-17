cce0a4a6072d1776b423b2075b5bd351d30e6931
Stacey <stacey.sych@gmail.com>
2021-05-17 16:18:00 +0300

    rewrite: http to express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { IncomingMessage, RequestListener, ServerResponse } from "http";
-
-const http = require('http');
+import { Request, Response } from 'express'
+const express = require('express');
+const app = express();
+const userName = process.env.USERNAME;
 
 const port = 3000;
-const host = 'localhost';
 const mockedData = [
     {
         "displayName": "Cyberpank 2077",
@@ -22,47 +22,14 @@ const mockedData = [
     }
 ];
 
-const requestHandler: RequestListener = (req: IncomingMessage, res: ServerResponse) => {
-
-    if(req.method === 'GET') {
-        switch(req.url) {
-            case '/products':
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify(mockedData));
-            break;
-            default: 
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write('<h1>My first server</h1>');
-        };
-        res.end();
-    }
-
-    if(req.method === 'POST') {
-        let data = '';
-        if(req.url === '/products') {
-            req.on('data', (chunk) => {
-                data += chunk;
-            });
-    
-            req.on('end', () => {
-                res.statusCode = 201;
-                res.setHeader('Content-Type', 'application/json');
-                mockedData.push(JSON.parse(data));
-                res.write(JSON.stringify(mockedData));
-                res.end();
-            });
-        } else {
-            res.end();
-        }
-    }
-    
-}
-const server = http.createServer(requestHandler);
-
-server.listen(port, host, (err) => {
-    if (err) {
-        return console.log('Something went wrong', err)
-    }
-
-    console.log(`Server is listening on ${port}`)
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Hello, ${userName}! My name is Anastasiya Sych. It's my first server`)
 });
+app.get('/products', (req: Request, res: Response) => {
+    res.json(mockedData);
+})
+
+
+app.listen(port, () => {
+    console.log(`Express is listening at http://localhost:${port}`)
+})
