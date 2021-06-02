d3b9e2743169080025f6d950eac98dd5abf153a6
Stacey <stacey.sych@gmail.com>
2021-06-02 16:52:13 +0300

    categories: handle errors typegoose

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from 'express'
-import { ObjectId } from 'mongoose';
+import { Request, Response } from 'express';
+import { ApiError } from '../utils/ApiError';
 import { CategoryService } from '../services';
 
 const { Router } = require('express');
@@ -8,10 +8,10 @@ const router = Router();
 
 router.get('/', async (req: Request, res: Response, next) => {
     try {
-        const data = await CategoryService.getAllCategories();
+        const data = await CategoryService.getAllCategories();        
         res.status(200).json(data);
     } catch(e) {
-        res.status(500).send({ "err": e })
+        next(e);
     }
 });
 
@@ -23,12 +23,22 @@ router.get('/:id', async (req: Request, res: Response, next) => {
         data = await CategoryService.getCategoryById(id);
 
         if(req.query.includeProducts || req.query.includeTop3Products) {
-            data = await CategoryService.getCategoryByIdWithProducts(id, req.query.includeProducts as string, req.query.includeTop3Products as string)
+            data = await CategoryService.getCategoryByIdWithProducts(id, req.query.includeProducts as string, req.query.includeTop3Products as string);
+
+            if(!data) {
+                next(ApiError.badRequest('Bad request. Your request should be the following format: id?includeProducts=true&includeTop3Products=true'));
+                return;
+            }
+        }
+
+        if(!data) {
+            next(ApiError.notFound(`Nothing was found for this ID - ${req.params.id}`));
+            return;
         }
 
         res.status(200).json(data);
     } catch(e) {
-        res.status(500).send({ "err": e })
+        next(e);
     }
 });
 
