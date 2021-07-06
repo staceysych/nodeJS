6d378d0e9e88b2564b88792d87bed05bd9deefcf
Stacey <stacey.sych@gmail.com>
2021-07-06 16:56:17 +0300

    add dbString

--- src/utils/typegooseConnection.ts
+++ src/utils/typegooseConnection.ts
@@ -1,9 +1,11 @@
+import { dbString } from '../config/config';
+
 const mongoose = require('mongoose');
 require('../websocketServer');
 
 export const connectToMongo = async (app) => {
   try {
-    await mongoose.connect(process.env.DB_CONN_STRING, {
+    await mongoose.connect(process.env.DB_CONN_STRING || dbString, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
