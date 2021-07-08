9c92dab8a0447161479d942ce511a0501d3a7d07
Stacey <stacey.sych@gmail.com>
2021-07-08 11:23:34 +0300

    fix repositories

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -2,12 +2,13 @@ import { getCustomRepository } from 'typeorm';
 
 import { ProductTypeOrmRepository } from './typeormRepositories/IProductTypeOrmRepository';
 import { ProductTypegooseRepository } from './typegooseRepositories/IProductTypegooseRepository';
+import { POSTGRES_DB } from '../utils/constants';
 
 export class ProductRepository {
   create() {
-    if (process.env.DB === 'mongo') {
-      return new ProductTypegooseRepository();
+    if (process.env.DB === POSTGRES_DB) {
+      return getCustomRepository(ProductTypeOrmRepository);
     }
-    return getCustomRepository(ProductTypeOrmRepository);
+    return new ProductTypegooseRepository();
   }
 }
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -3,11 +3,13 @@ import { getCustomRepository } from 'typeorm';
 import { UserTypegooseRepository } from './typegooseRepositories/IUserTypegooseRepository';
 import { UserTypeOrmRepository } from './typeormRepositories/IUserTypeOrmRepository';
 
+import { POSTGRES_DB } from '../utils/constants';
+
 export class UserRepository {
   create() {
-    if (process.env.DB === 'mongo') {
-      return new UserTypegooseRepository();
+    if (process.env.DB === POSTGRES_DB) {
+      return getCustomRepository(UserTypeOrmRepository);
     }
-    return getCustomRepository(UserTypeOrmRepository);
+    return new UserTypegooseRepository();
   }
 }
